fba4990e35aec7b91ceff82e44fffe05d884062a
mr andersen <chasea523@gmail.com>
2019-11-12 17:32:33 -0800

    created 'generate' function

--- script.js
+++ script.js
@@ -1,2 +1,31 @@
-how many characters do you want?
-how many special characters?
+
+
+
+
+
+
+//generate random password
+
+
+function generate(){
+
+    //set password length/complexity
+    let complexity = document.getElementById("customRange1").value;
+
+    //values for password
+    let characters = "ABCDEFGHIJKLMNOPQRSTUVWZYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
+
+    let password = "";
+
+    //create for loop to choose password characters
+    for(var i = 0; i <= complexity; i++){
+        password = password + characters.charAt(Math.floor(Math.random() * Math.floor(characters.length - 1)));
+    }
+
+    //add password to textbox/display area
+    document.getElementById("display").value = password;
+
+    //add password to previously generated passwords section
+    document.getElementById("lastNums").innerHTML += password + "<br/>";
+
+}
